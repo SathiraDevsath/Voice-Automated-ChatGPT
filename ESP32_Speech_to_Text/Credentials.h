4bba03760ef14eccbbf1107efe778038f399348e
Sathira Perera <161849428+SathiraDevsath@users.noreply.github.com>
2024-03-14 15:23:03 +0530

    Add files via upload

---
+++ ESP32_Speech_to_Text/Credentials.h
@@ -0,0 +1,18 @@
+// Provide all the crdentials here
+
+
+
+// Your WiFi Credentials 
+const char *ssid = "auincubation";       // WiFi SSID Name
+const char *password = "auic@XYZ123";// WiFi Password ( Keep it blank if your WiFi router is open )
+
+
+// Parameters of Google Speech to Text API
+const String ApiKey = "AIzaSyC8wd5zEEx-HFCOpdkl1Y7S5HD6pVFeuLY";// Google Speech to Text API key
+String SpeechtoText_Language = "en-IN"; // Language 
+
+// Parameters of OpenAI API 
+const char* chatgpt_token = "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"; // OpenAI API Key
+String OpenAI_Model = "gpt-3.5-turbo-instruct"; // Model
+String OpenAI_Temperature = "0.20"; // temperature
+String OpenAI_Max_Tokens = "40"; //Max Tokens 
