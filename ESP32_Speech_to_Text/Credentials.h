7a7eccd09b6753429c479d2d1be063de3115e1be
Sathira Perera <161849428+SathiraDevsath@users.noreply.github.com>
2024-03-14 15:39:50 +0530

    Credentials.h

--- ESP32_Speech_to_Text/Credentials.h
+++ ESP32_Speech_to_Text/Credentials.h
@@ -8,11 +8,11 @@ const char *password = "auic@XYZ123";// WiFi Password ( Keep it blank if your Wi
 
 
 // Parameters of Google Speech to Text API
-const String ApiKey = "AIzaSyC8wd5zEEx-HFCOpdkl1Y7S5HD6pVFeuLY";// Google Speech to Text API key
+const String ApiKey = "            ";// Google Speech to Text API key
 String SpeechtoText_Language = "en-IN"; // Language 
 
 // Parameters of OpenAI API 
-const char* chatgpt_token = "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"; // OpenAI API Key
+const char* chatgpt_token = "                   "; // OpenAI API Key
 String OpenAI_Model = "gpt-3.5-turbo-instruct"; // Model
 String OpenAI_Temperature = "0.20"; // temperature
 String OpenAI_Max_Tokens = "40"; //Max Tokens 
